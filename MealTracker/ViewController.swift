2abf98d247bcf59fd5d56679d487d9ac0ba06e5b
Yang Yongli <yongli.yang@dianping.com>
2016-06-19 16:34:28 +0800

    Initial Commit

---
+++ MealTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MealTracker
+//
+//  Created by Yang Yongli on 6/19/16.
+//  Copyright © 2016 Yang Yongli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
